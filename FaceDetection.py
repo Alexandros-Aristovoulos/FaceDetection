b750d80030c5921ea7fbc21164c80712f8d7aee3
Αλέξανδρος Αριστόβουλος <alexandrosaristovoulos@gmail.com>
2020-10-22 00:51:06 +0300

    Fix right faces

--- FaceDetection.py
+++ FaceDetection.py
@@ -9,21 +9,25 @@ def main():
     # Load the cascade
     front_face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     profile_face_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
-
+    
     # To capture video from webcam. 
     cap = cv2.VideoCapture(0)
-    # To use a video file as input 
-    # cap = cv2.VideoCapture('filename.mp4')
+    
+    #check if camera is detected
+    if(cap.isOpened() == False):
+        print("Could not detect a camera")
 
-    while True:
+
+    while cap.isOpened():
         # Read the frame
-        _, img = cap.read()
-        img = findFaces(img, front_face_cascade, profile_face_cascade)
+        _, image = cap.read()
+        #find the faces
+        image = findFaces(image, front_face_cascade, profile_face_cascade)
 
         #to make a resizeable window
         #cv2.namedWindow('image', cv2.WINDOW_NORMAL)
         # Display
-        cv2.imshow('image', img)
+        cv2.imshow('Face Detection', image)
         # Stop if escape key is pressed
         k = cv2.waitKey(30) & 0xff
         if k==27:
@@ -37,14 +41,26 @@ def findFaces(image, front_face_cascade, profile_face_cascade):
     #gray it
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #Find the faxes using the model
-    front_faces = front_face_cascade.detectMultiScale(gray, 1.3, 4)
-    profile_faces = profile_face_cascade.detectMultiScale(gray, 1.3, 4)
+    front_faces = front_face_cascade.detectMultiScale(gray, 1.3, 5)
+    left_profile_faces = profile_face_cascade.detectMultiScale(gray, 1.3, 5)
+    flipped = cv2.flip(gray, 1)
+    right_profile_faces = profile_face_cascade.detectMultiScale(flipped, 1.3, 5)
+   
 
-    #draw green box around faces
+    #Front Face
     for (x,y,w,h) in front_faces:
-        image = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0), 2)
-    for (x,y,w,h) in profile_faces:
-        image = cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0), 2)
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0), 2)
+        
+    #Left Faces
+    for (x,y,w,h) in left_profile_faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0), 2)
+
+    #Right Faces (we use the fliped image)
+    for (x,y,w,h) in right_profile_faces:
+        image = cv2.flip(image,1)
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255), 2)               
+        image = cv2.flip(image,1)
+
     return image
 
 def displayImage(img):
