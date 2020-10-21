d066da7e62a68e6ed41b7fc054f96da6e17039c9
Αλέξανδρος Αριστόβουλος <alexandrosaristovoulos@gmail.com>
2020-10-21 21:32:21 +0300

    find in video

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,27 +1,51 @@
-
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-
-
 def main():
-    img = findFaces("test.jpg")
-    displayImage(img)
+    #img = findFaces("test.jpg")
+    #displayImage(img)
+
+    # Load the cascade
+    front_face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    profile_face_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+
+    # To capture video from webcam. 
+    cap = cv2.VideoCapture(0)
+    # To use a video file as input 
+    # cap = cv2.VideoCapture('filename.mp4')
+
+    while True:
+        # Read the frame
+        _, img = cap.read()
+        img = findFaces(img, front_face_cascade, profile_face_cascade)
+
+        #to make a resizeable window
+        #cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        # Display
+        cv2.imshow('image', img)
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k==27:
+            break
+    # Release the VideoCapture object
+    cap.release()
+
+
         
-def findFaces(imageFileName):
-    #get the model
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    #read the image
-    img = cv2.imread(imageFileName)
+def findFaces(image, front_face_cascade, profile_face_cascade):    
     #gray it
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #Find the faxes using the model
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    front_faces = front_face_cascade.detectMultiScale(gray, 1.3, 4)
+    profile_faces = profile_face_cascade.detectMultiScale(gray, 1.3, 4)
+
     #draw green box around faces
-    for (x,y,w,h) in faces:
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
-    return img
+    for (x,y,w,h) in front_faces:
+        image = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0), 2)
+    for (x,y,w,h) in profile_faces:
+        image = cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0), 2)
+    return image
 
 def displayImage(img):
     #to make a resizeable window
@@ -30,6 +54,7 @@ def displayImage(img):
     cv2.imshow('image',img)
     cv2.waitKey()
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
